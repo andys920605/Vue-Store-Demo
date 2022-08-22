22575040ec24b5e9154b45f19d46bc9c62a8a8fb
Andy <andy_chang@protech.com.tw>
2022-08-22 16:40:02 +0800

    init vue

---
+++ src/views/HomeView.vue
@@ -0,0 +1,18 @@
+<script setup>
+import TheWelcome from '@/components/TheWelcome.vue'
+</script>
+
+<template>
+    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+
+      <nav>
+        <RouterLink to="/login">Login</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+
+  <RouterView />
+</template>
