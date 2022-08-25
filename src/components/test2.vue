b827d96902ad2ede8ba2cbb5f2811ea05a875c32
je <qwer90218@gmail.com>
2022-08-25 22:52:14 +0800

    更改

--- src/components/test2.vue
+++ src/components/test2.vue
@@ -3,6 +3,7 @@
 
 <template>
     <h1>Hi here is 2</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quis dignissimos voluptatem in maiores autem id non animi fugit doloribus repellat deserunt facilis provident, consequatur esse ipsam obcaecati porro cumque aspernatur!</p>
 </template>
 
 <script>
