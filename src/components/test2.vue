b94297a1e6f80bace71f3146eb41e7e7d17749cc
je <qwer90218@gmail.com>
2022-08-25 23:04:41 +0800

    更改

--- src/components/test2.vue
+++ src/components/test2.vue
@@ -3,8 +3,7 @@
 
 <template>
     <h1>Hi here is 2</h1>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quis dignissimos voluptatem in maiores autem id non animi fugit doloribus repellat deserunt facilis provident, consequatur esse ipsam obcaecati porro cumque aspernatur!</p>
-    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Excepturi veniam perspiciatis dolor maxime qui necessitatibus aliquam aspernatur nulla placeat exercitationem optio consectetur voluptatem ea nostrum, laboriosam iure porro est modi?</p>
+    <p>測試</p>
 </template>
 
 <script>
