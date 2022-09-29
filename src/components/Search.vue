7cdab21858f7d69af6339c919abd510cb2ea6fb3
Andy <andy_chang@protech.com.tw>
2022-09-29 09:25:35 +0800

    reverse merge

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -10,35 +10,20 @@
         狀態3.搜尋後返回
         狀態4.返回以及篩選-->
 
-<<<<<<< HEAD
-        <div class="search-box">
-            <i class="fa-solid fa-chevron-left"></i>
-            <div class="search-item">
-                <div class="search-icon">
-                    <i class="fa-solid fa-magnifying-glass"></i>
-                </div>
-                <span>搜尋你想找的商品</span>
-=======
     <div class="search-box">
         <div v-bind:class="searchItem">
             <div class="search-icon">
                 <i class="fa-solid fa-magnifying-glass"></i>
->>>>>>> c81941026b6f98bd69b262ce2a1556b8ae39bf75
             </div>
-    
-        <RouterLink to="/the-welcome">
-            <button class="search-btn">登入</button>
-        </RouterLink>
-        <div class="filter">
-            <i class="fa-solid fa-filter"></i>
-            <span>篩選</span>
+            <span>搜尋你想找的商品</span>
         </div>
+        <button class="search-btn" v-show="showLoginBtn">登入</button>
     </div>
     
 </template>
 
-<<<<<<< HEAD
-=======
+
+
 <script>
     // 模擬登入後拿到的token 
     var accessToken ={
@@ -86,14 +71,10 @@
 </script>
 
 
-
-
->>>>>>> c81941026b6f98bd69b262ce2a1556b8ae39bf75
 <style>
     .search-box{
-        margin: 0px 0 10px;
+        margin: 45px 0 30px;
         display: flex;
-        align-items: center;
     }
     .search-item{
         margin: 0 20px;
@@ -104,8 +85,17 @@
         display: flex;
         color: gray;
     }
+    .search-item-login{
+        margin: 0 20px;
+        height: 45px;
+        width: 100%;
+        background: white;
+        border-radius: 30px;
+        display: flex;
+        color: gray;
+    }
     span{
-        font-size: 16px;
+        font-size: 20px;
         padding-left: 10px;
         line-height: 45px;
     }
@@ -121,54 +111,12 @@
         background: none;
         border: none;
     }
-    .search-box .fa-chevron-left{
-    display: none;
-    }
-    .filter{
-        display: none;
-    }
 
-    /* 狀態2.移除登入按鈕僅有搜尋欄（CSS註解處）*/
-    /* .search-btn{
+    /* 搜尋欄登入後變化
+    .search-btn{
         display: none;
     }
     .search-item{
         width: 100%;
-    }  */
-
-    /* 狀態3.搜尋後返回*/
-    /* .search-btn{
-        display: none;
-    }
-    .search-item{
-        width: 100%;
-    } 
-    .search-box .fa-chevron-left{
-        padding-left: 15px; 
-        font-size: 30px;
-        color: rgb(62, 62, 62);
-    }
-    .search-item{
-        margin-left: 10px;
-    } */
-    
-    /* 狀態4.返回以及篩選*/
-    /* .filter{
-        display: flex;
-        background: white;
-        padding: 0px 12px;
-        align-items: center;
-        border-radius: 40px;
-        color: rgb(62, 62, 62);
-    }
-    .filter span{
-        font-size: 14px;
-    }
-    .filter .fa-filter{
-        font-size: 18px;
-    }
-    .search-item{
-    width: 60%;
-    margin-right: 10px;
     } */
 </style>
