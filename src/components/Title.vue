5517957e2a4b384c53728c46bfcde6f0bfc59ac5
je <qwer90218@gmail.com>
2022-10-01 16:43:35 +0800

    新增標題元件

---
+++ src/components/Title.vue
@@ -0,0 +1,22 @@
+
+
+<template>
+
+    <h1>{{ name }}</h1>
+
+</template>
+
+<script>
+export default {
+    name: "Title",
+    props:['name']
+};
+</script>
+
+<style>
+h1{
+    font-size: 28px;
+    margin: 40px 30px 20px;
+    font-weight: 500;
+}
+</style>
